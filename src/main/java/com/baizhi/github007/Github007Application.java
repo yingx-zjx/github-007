ef27d0a257011dabaa9b64dfb6ba4529353b5064
zhujx <2055828139@qq.com>
2020-07-17 01:53:12 +0800

    这是第一个github项目做个说明

---
+++ src/main/java/com/baizhi/github007/Github007Application.java
@@ -0,0 +1,13 @@
+package com.baizhi.github007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Github007Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Github007Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/baizhi/github007/Github007ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.baizhi.github007;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Github007ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("这是第一个github程序开发");
+    }
+
+}
